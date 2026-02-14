ba9a009dfa872a87e3e9df8f8bb64b8285591c60
Hanzar Nioo <hnioo@z2t13c1.42bangkok.com>
2026-02-15 02:56:02 +0700

    Need to fix a little bit

--- main.c
+++ main.c
@@ -12,6 +12,26 @@
 
 #include "philo.h"
 
+void	ft_freeall(t_philo *philo, pthread_mutex_t *forks,
+	pthread_mutex_t *death)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->number_of_philo)
+	{
+		pthread_mutex_destroy(philo[i].left_fork);
+		pthread_mutex_destroy(philo[i].right_fork);
+		i++;
+	}
+	pthread_mutex_destroy(philo->death);
+	pthread_mutex_destroy(forks);
+	free(philo->check_dead);
+	free(forks);
+	free(death);
+	free(philo);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo			*philo;
@@ -26,5 +46,7 @@ int	main(int argc, char **argv)
 		error(MALLOC);
 	philo_init(argc, argv, philo);
 	mutex_init(philo, forks, death);
+	create_thread(philo);
+	ft_freeall(philo, forks, death);
 	return (0);
 }
